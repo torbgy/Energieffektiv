d7a3dfa4f92811f3c9ae796173c17f80579e16b5
Your Name <you@example.com>
2015-04-21 14:02:58 +0200

    snake progress

--- ovinger/Oving3/game-1.0/game.c
+++ ovinger/Oving3/game-1.0/game.c
@@ -21,52 +21,125 @@
     Bits per pixel: 16 ( 5 red, 5 blue, 6 green ) 
 
 */
+typedef struct {
+    int x;
+    int y;
+}pos;
 
 void display_print_info(struct fb_var_screeninfo vinfo,struct fb_fix_screeninfo finfo);
 
 int main(int argc, char *argv[])
 {
 	int file_descriptor = open("/dev/fb0", O_RDWR);
-	
+	int i,x,y; // counters
+
 	struct fb_var_screeninfo vinfo;
 	struct fb_fix_screeninfo finfo;
 	
+    // Info
 	ioctl(file_descriptor, FBIOGET_FSCREENINFO, &finfo);
 	ioctl(file_descriptor, FBIOGET_VSCREENINFO, &vinfo);
-	
-	uint16_t* display = (uint16_t*)mmap(0,finfo.smem_len, PROT_WRITE|PROT_READ, MAP_SHARED, file_descriptor, 0);
-	
-	
 	display_print_info(vinfo,finfo);
-	
-	
-	  
-    struct fb_copyarea rect;
-    rect.dx = 0;
-    rect.dy = 0;
-    rect.width = 320;
-    rect.height = 240;
+    // Initialize a black screen & memory-map
+	uint16_t* display = (uint16_t*)mmap(0,finfo.smem_len, PROT_WRITE|PROT_READ, MAP_SHARED, file_descriptor, 0);
+	struct fb_copyarea rect = {dx:0,dy:0,width:320,height:240};
+    for (x=0;x<320;x++){
+        for(y=0;y<240;y++){
+            display[320*y + x] = 0x0000;
+        }
+    }
+    ioctl(file_descriptor, 0x4680, &rect);
     
-    int y, x;
-    printf("game: before the loop\n");
-    while(1){
-        for( y = rect.dy ; y < rect.height+rect.dy ; y++){
-            for( x = rect.dx ; x < rect.width+rect.dx ; x++){
+
+
+    int level = 1; 
+    int quit = 1;
+
+    // Dynamic snake 
+    int snakeLength = 0;
+    pos* snake = malloc(snakeLength+1*sizeof(pos));
+
+
+    // Positions + initial snake
+    pos dir = {x:1,y:0};
+    pos apple = { 160,140 };
+    pos next = { 160,120 };
+    rect.width = 10; 
+    rect.height = 10;
+
+    rect.dx = next.x;
+    rect.dy = next.y;
+
+    // Starting the game
+    printf("Initializing game\n");
+    while(quit != 0){
+
+        next.x = next.x + 10*dir.x;
+        next.y = next.y + 10*dir.y;
+
+        // In case it's running offscreen -> aka go trough the wall
+        if(next.y >= 240 || next.y < 0){         
+            next.y = abs(next.y - 240);
+        }else if(next.x >= 320 || next.x < 0){    
+            next.x = abs(next.x - 320);
+        }
+
+        
+        // 3 different cases
+        if ( display[next.y*320+next.x] == 0xffff){
+
+            printf("Game over!\n");
+            break;
+        }
+        else if(next.x == apple.x && next.y == apple.y){
+
+            snakeLength++;
+
             
-                display[320*y + x] += 0x0001;
-            
+
+        }
+        else{
+
+            // Delete tail visual
+            rect.dx = snake[snakeLength].x;
+            rect.dy = snake[snakeLength].y;
+            for(x=rect.dx; x<rect.dx+10; x++){
+                for(y=rect.dy; y<rect.dy+10;y++){
+                    display[320*y + x] = 0x0000;
+                }
+            }
+            ioctl(file_descriptor, 0x4680, &rect);
+
+            //Update snake
+            for (i=snakeLength;i>0;i--){
+                snake[i] = snake[i-1];
+            }
+            snake[0] = next;
+
+
+        }
+        //Generate head
+        rect.dx = next.x;
+        rect.dy = next.y;
+        for(x=rect.dx; x<rect.dx+9; x++){
+            for(y=rect.dy; y<rect.dy+9;y++){
+                display[320*y + x] = 0xffff;
             }
         }
-        
-     
         ioctl(file_descriptor, 0x4680, &rect);
-        
-        //sleep(1);
+
+        printf("This is now the snake:\n");
+        for (i=0;i<snakeLength+1;i++){
+            printf("x:%d, y:%d \n",snake[i].x,snake[i].y );
+        }
+
+
+        sleep(1/level);
     }
 
 
-
-    munmap(display,320*240);
+    free(snake);
+    munmap(display,finfo.smem_len);
     close(file_descriptor);
 	exit(EXIT_SUCCESS);
 }
@@ -85,3 +158,14 @@ void display_print_info(struct fb_var_screeninfo vinfo,struct fb_fix_screeninfo 
     printf("height:         %d \n", vinfo.height);
     printf("width:          %d \n", vinfo.width); 
 }
+
+
+/*
+void drawApple(int file_descriptor, int x, int y){
+
+
+
+
+}
+*/
+

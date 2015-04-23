d28bd6654d4a1b79a5e9bf033bc4a50509549613
CARD student dummy account <cardstud@dmlab09.(none)>
2015-04-23 18:09:35 +0200

    final! working properly

--- ovinger/Oving3/driver-gamepad-1.0/driver-gamepad.c
+++ ovinger/Oving3/driver-gamepad-1.0/driver-gamepad.c
@@ -12,49 +12,47 @@
 #include <linux/device.h>
 #include <linux/irqreturn.h>
 #include <linux/moduleparam.h>
-//For platform drivers....
-//#include <linux/platform_device.h>
+// Signals
+#include <asm/siginfo.h> // siginfo
+#include <asm/signal.h>	
+#include <linux/sched.h> // find_task_by_pid_type 
+#include <linux/rcupdate.h> // rcu_read_lock
+#include <linux/pid.h> // PID-ns
+
 #include "efm32gg.h"
 
 #define DRIVER_NAME "gamepad"
+#define SIG_NUM 44 //Signal number
 
-MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("Platform driver module demo");
-//MODULE_DEVICE_TABLE(of, my_of_match);
 
-typedef unsigned char uint8_t;
-typedef unsigned short uint16_t;
-typedef unsigned int uint32_t;
 
 /*Base functions */
 static int __init ourinitmodule(void);
 static void __exit ourcleanupmodule(void);
-//static int my_probe(struct platform_device *dev);
-//static int my_remove(struct platform_device *dev);
-static int my_open(struct inode *inode, struct file *filp);
-static int my_release(struct inode *inode, struct file *filp);
-static ssize_t my_read(struct file *filp, char __user *buff, size_t count, loff_t *offp);
-static irqreturn_t interrupt_handler(int irq, void *dev_id, struct pt_regs *regs);
-
+static int gamepad_open(struct inode*, struct file*);
+static int gamepad_release(struct inode*, struct file*);
+static irqreturn_t interrupt_handler(int, void*, struct pt_regs*);
+static ssize_t gamepad_write(struct file*,const char __user*,size_t,loff_t*);
 
 static struct file_operations my_fops = {
 	.owner = THIS_MODULE,
-	.read = my_read,
-	.open = my_open,
-	.release = my_release
+	.write = gamepad_write,
+	.open = gamepad_open,
+	.release = gamepad_release	
 };
 
 struct cdev my_cdev;
 struct class *cl;
 dev_t devno = 1;
-
 void __iomem *gpio;
-uint8_t button;
 
-//******NOTE*****
-//ARM = mmap all func support, ports unsigned int
+// Signals
+int game_pid = 0;
+struct siginfo info;
+struct task_struct *t; 
+
 static int __init ourinitmodule(void){
-	printk(KERN_INFO "\n Welcome to sample Platform driver... \n");
+	
 
 	// Allocate memory for the device
 	alloc_chrdev_region(&devno,0,1, DRIVER_NAME);
@@ -62,147 +60,96 @@ static int __init ourinitmodule(void){
 	// Memory setup
 	check_mem_region(GPIO_BASE, GPIO_SIZE);
 	request_mem_region(GPIO_BASE,GPIO_SIZE, DRIVER_NAME);
+	// Dynamic memory
 	gpio = GPIO_BASE;
-	// Dynamic Memory setup
-	ioremap_nocache(gpio, GPIO_SIZE);
-
+	ioremap_nocache(gpio,GPIO_SIZE);
 	// GPIO setup
 	iowrite32(0x33333333, GPIO_PC_MODEL);
 	iowrite32(0xff, GPIO_PC_DOUT);
 	iowrite32(0x22222222, GPIO_EXTIPSELL);
 
-
-
 	// Interrupt setup
-	iowrite32(0xff, GPIO_IEN);
 	iowrite32(0xff, GPIO_IFC);
+	iowrite32(0xff, GPIO_IEN);
 	iowrite32(0xFF, GPIO_EXTIFALL);
-	request_irq(17, (irq_handler_t)interrupt_handler, 0, DRIVER_NAME, &my_cdev); //17 & 18 = GPIO EVEN & ODD
-	request_irq(18, (irq_handler_t)interrupt_handler, 0, DRIVER_NAME, &my_cdev);
+	request_irq(17, (irq_handler_t)interrupt_handler, 0, DRIVER_NAME, NULL); //17 & 18 = GPIO EVEN & ODD
+	request_irq(18, (irq_handler_t)interrupt_handler, 0, DRIVER_NAME, NULL);
 
-	
+
+	// Signals setup
+	memset(&info,0,sizeof(struct siginfo));
+	info.si_signo = SIG_NUM;
+	info.si_code = SI_QUEUE;
+
+
 	// Actives the driver
-	cdev_init(&my_cdev,&my_fops); 
-	if(cdev_add(&my_cdev, devno, 1) < 0){
-		printk(KERN_ERR "\n driver-gamepad: cdev_add failed \n");
-
-	}
-	my_cdev.owner = THIS_MODULE;
-	cdev_add(&my_cdev, devno, 1);
-	//platform_driver_register(&my_driver);
 	cl = class_create(THIS_MODULE, DRIVER_NAME);
 	device_create(cl,NULL,devno,NULL, DRIVER_NAME);
+
+	cdev_init(&my_cdev,&my_fops);
+	my_cdev.owner = THIS_MODULE;
+	cdev_add(&my_cdev, devno, 1);
+
 
 
 	return 0;
 }
 
 static void __exit ourcleanupmodule(void){
-	printk(KERN_INFO "\n Thanks... Exiting sample Platform driver... \n");
-	/* Unregistering from Kernel */
-	//platform_driver_unregister(&my_driver);
-	//Clean drivers
+
+	// Unregister Device
 	device_destroy(cl, devno);
 	class_destroy(cl);
-
 	cdev_del(&my_cdev);
 
-	//Release interrupt
+	// Release interrupt
 	free_irq(17, NULL);
 	free_irq(18, NULL);
 
-	//GPIO
-	iounmap(gpio);
+	// Release GPIO
 	release_mem_region(GPIO_BASE, GPIO_SIZE);
 	unregister_chrdev_region(&devno,1);
-
+	iounmap(gpio);
 	return;
 }
 
-/*static int my_probe(struct platform_device *dev){
-	//Empty probe function
-}
-
-static int my_remove(struct platform_device *dev){
-	//Empty remove function
-}*/
-
-
-static ssize_t my_read(struct file *filp, char __user *buff, size_t count, loff_t *offp){
-	copy_to_user(buff, &button, 1);
-	return 1;
-}
-
-
-
 
 static irqreturn_t interrupt_handler(int irq, void *dev_id, struct pt_regs * regs){
-
-	uint32_t input;
+ 
+	info.si_int = ioread32(GPIO_PC_DIN);
+	send_sig_info(SIG_NUM,&info,t); 
 	iowrite32(0xff, GPIO_IFC);
-	input = ioread32(GPIO_PC_DIN) | 0xff00;
-	
-	switch(input){
-		case (0xfffe) :
-			button = 1;
-			break;
-		case (0xfffd) :
-			button = 4;
-			break;
-		case (0xfffb) :
-			button = 3;
-			break;
-		case (0xfff7) :
-			button = 2;
-			break;
-		case (0xffef) :
-			button = 5;
-			break;
-		case (0xffdf) :
-			button = 6;
-			break;
-		case (0xffbf) :
-			button = 7;
-			break;
-		case (0xff7f) :
-			button = 8;
-			break;
-	}
-
 
 	return IRQ_HANDLED;
 }
 
+static ssize_t gamepad_write(struct file *filp,const char __user *buf,size_t count,loff_t *ppos){
+	char mybuf[10];
+	copy_from_user(mybuf,buf,count);
+	sscanf(mybuf,"%d",&game_pid);
+	rcu_read_lock();
+	t = pid_task(find_pid_ns(game_pid, &init_pid_ns),PIDTYPE_PID);
+	if(t == NULL){
+		printk("Error in gamepad_write\n");
+		rcu_read_unlock();
+		return -1;
+	}
+	rcu_read_unlock();
+	
 
-static int my_open(struct inode *inode, struct file *filp){
+
 	return 0;
 }
 
-static int my_release(struct inode *inode, struct file *filp){
+static int gamepad_open(struct inode *inode, struct file *filp){
 	return 0;
 }
 
-/*static struct platform_driver my_driver = { //my driver decleared not used
-	.probe = my_probe,
-	.remove = my_remove,
-	.driver = {
-		.name = my, //undecleared
-		.owner = THIS_MODULE,
-		.of_match_table = my_of_match, //undecleared
-	},
-};
-
-static const struct of_device_id my_of_match[] = {
-	{
-		.compatible = "tdt4258",
-	},
-	{},
-};*/
+static int gamepad_release(struct inode *inode, struct file *filp){
+	return 0;
+}
 
 module_init(ourinitmodule);
 module_exit(ourcleanupmodule);
-
-/* Specifying my resources information */
-//struct resource *res = platform_get_resource(dev, IORESOURCE_MEM, index); //dev, index undecleared
-
-//int irq = platform_get_irq(dev, index); //
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("Gamepad Driver");
--- ovinger/Oving3/game-1.0/game.c
+++ ovinger/Oving3/game-1.0/game.c
@@ -13,186 +13,274 @@
 #include <sys/mman.h>
 //sleep
 #include <unistd.h>
-//pow()
-#include <math.h>
+//signals
+#include <linux/fs.h>
+#include <fcntl.h>
+#include <signal.h>
+#define SIG_NUM 44 // Signal number
 
-/*
-    Screen size: 320x240
-    Bits per pixel: 16 ( 5 red, 5 blue, 6 green ) 
-
-*/
 typedef struct {
     int x;
     int y;
 }pos;
 
-void readGamepad(int fddriver,pos *dir);
-void newApple(uint16_t* display,int fdrandom, int fdfb,pos* apple,pos* next, struct fb_copyarea *rect);
 
+void sig_handler(int sig_num, siginfo_t *info,void *unused);
+void newApple(uint16_t* display,int fd_random, int fd_fb,pos* apple,pos* next, struct fb_copyarea *rect);
+
+pos dir = {1,0};
+int speed = 1; 
+int reset = 0;
+int pauseGame = 1;
+int fd_gamepad;
+int fd_fb;
+int fd_random;
 
 int main(int argc, char *argv[])
 {   
-    printf("Initializing game\n");
+
     //Devices to use
-	int fdfb = open("/dev/fb0", O_RDWR);
-    int fdrandom = open("/dev/urandom", O_RDONLY);
-    int fddriver = open("/dev/gamepad", O_RDONLY);
+	fd_fb = open("/dev/fb0", O_RDWR);
+    fd_random = open("/dev/urandom", O_RDONLY);
+    fd_gamepad = open("/dev/gamepad", O_WRONLY);
 
+    if (fd_gamepad < 0 || fd_random < 0 || fd_fb < 0){
+        printf("game: Error opening one of the drivers\n");
 
-    // Counters
-	int x,y; 
-//read(fdfb,buffer,"#bytes")
-
-    // Initialize a black screen & memory-map
-	uint16_t* display = (uint16_t*)mmap(0,320*240*2, PROT_WRITE|PROT_READ, MAP_SHARED, fdfb, 0);
-	struct fb_copyarea rect = {dx:0,dy:0,width:320,height:240};
-    for (x=0;x<320;x++){
-        for(y=0;y<240;y++){
-            display[320*y + x] = 0x0000;
-        }
     }
-    ioctl(fdfb, 0x4680, &rect);
-    
+    // Memory Map
+    uint16_t* display = (uint16_t*)mmap(0,320*240*2, PROT_READ | PROT_WRITE, MAP_SHARED, fd_fb, 0);
+    // Signals
+    struct sigaction sig;
+    sig.sa_sigaction = sig_handler;
+    sig.sa_flags = SA_SIGINFO;
+    sigaction(SIG_NUM,&sig,NULL);
+    char buf[10];
+    sprintf(buf,"%i",getpid());
+    write(fd_gamepad,buf,11);
 
-    // Internals
-    int speed = 2; 
-    int quit = 1;
-
-    // Dynamic snake 
-    unsigned int snakeLength = 0;
-    pos* snake = malloc(snakeLength+1*sizeof(pos));
-
-
-    // Positions + initial snake
-    pos dir = {1,0};
+    // Counters & internals
+	int x,y,i; 
+    pos snake[400];
+    unsigned int snakeLength;
+    unsigned int snakeTail;
+    struct fb_copyarea rect;
+    pos next;
     pos apple;
-    pos next = { 160,120 };
-    rect.width = 10; 
-    rect.height = 10;
-    newApple(display,fdrandom,fdfb,&apple,&next,&rect);
 
+    while(1){
 
-
-    // Starting the game
-    while(quit != 0){
-
-        
-
-        
-        readGamepad(fddriver, &dir);
-
-
-        next.x = next.x + 10*dir.x;
-        next.y = next.y + 10*dir.y;
-
-        // In case it's running offscreen -> aka go trough the wall
-        if(next.y >= 240 || next.y < 0){         
-            next.y = abs(abs(next.y) - 240);
-        }else if(next.x >= 320 || next.x < 0){    
-            next.x = abs(abs(next.x) - 320);
-        }
-
-        
-        // 3 different cases
-        if ( display[next.y*320+next.x] == 0xffff){
-
-            printf("Game over!\n");
-            break;
-        }
-        else if(next.x == apple.x && next.y == apple.y){
-            // Create a new apple
-            newApple(display,fdrandom,fdfb,&apple,&next,&rect);
-        
-
-            // Update snake
-            snakeLength++;
-            realloc(snake,snakeLength*sizeof(pos));
-            for (x=snakeLength;x>0;x--){
-                snake[x] = snake[x-1];
+        reset = 0;
+        pauseGame = 1;
+        snakeLength = 0;
+        snakeTail = 0;
+        // Initialize a black screen	
+        rect.dx = 0;
+        rect.dy = 0;
+        rect.width = 320;
+        rect.height = 240;
+        for (x=0;x<320;x++){
+            for(y=0;y<240;y++){
+                display[320*y + x] = 0x0000;
             }
-            snake[0]=next;
         }
-        else{
 
-            // Delete tail visual
-            rect.dx = snake[snakeLength].x;
-            rect.dy = snake[snakeLength].y;
-            for(x=rect.dx; x<rect.dx+10; x++){
-                for(y=rect.dy; y<rect.dy+10;y++){
-                    display[320*y + x] = 0x0000;
+        //Initial Snake;
+        next.x = 140;
+        next.y = 120;
+        for (i=0;i<5;i++){
+
+            next.x = next.x + 10*dir.x;
+            next.y = next.y + 10*dir.y;
+
+            snake[i] = next;
+            snakeLength++;
+
+            for(x=next.x; x<next.x+9; x++){
+                for(y=next.y; y<next.y+8;y++){
+                    display[320*y + x] = 0xffff;
+                }
+            }            
+        }
+        ioctl(fd_fb, 0x4680, &rect);
+
+        // Initial apple
+        rect.width = 10; 
+        rect.height = 10;
+        newApple(display,fd_random,fd_fb,&apple,&next,&rect);
+
+
+
+        // Starting the game
+        while(reset == 0){        
+
+
+            next.x = next.x + 10*dir.x;
+            next.y = next.y + 10*dir.y;
+
+            // In case it's running offscreen -> aka go trough the wall
+            if(next.y >= 240 || next.y < 0){         
+                next.y = abs(abs(next.y) - 240);
+            }else if(next.x >= 320 || next.x < 0){    
+                next.x = abs(abs(next.x) - 320);
+            }
+
+            
+            // 3 different cases
+            if ( display[next.y*320+next.x] == 0xffff){
+
+                pauseGame = 1;
+                reset = 1;
+               
+            }
+            else if(next.x == apple.x && next.y == apple.y){
+                // Create a new apple
+                newApple(display,fd_random,fd_fb,&apple,&next,&rect);
+            
+
+                // Update snake
+                snakeLength++;
+                
+                if ((snakeTail + snakeLength - 1) < 400){
+                    snake[snakeTail + snakeLength -1 ] = next;
+                }else{
+                    snake[snakeTail + snakeLength -1 - 400] = next;
+                }
+
+            }
+            else{
+
+                // Delete tail visual
+                rect.dx = snake[snakeTail].x;
+                rect.dy = snake[snakeTail].y;
+                for(x=rect.dx; x<rect.dx+10; x++){
+                    for(y=rect.dy; y<rect.dy+10;y++){
+                        display[320*y + x] = 0x0000;
+                    }
+                }
+                ioctl(fd_fb, 0x4680, &rect);
+
+                //Update snake
+                snakeTail++;
+                if (snakeTail>400-1){
+                    snakeTail = 0;
+                }
+                if ((snakeTail + snakeLength - 1) < 400){
+                    snake[snakeTail + snakeLength -1 ] = next;
+                }else{
+                    snake[snakeTail + snakeLength -1 - 400] = next;
+                }
+
+            }
+            //Generate head
+            rect.dx = next.x;
+            rect.dy = next.y;
+            for(x=rect.dx; x<rect.dx+9; x++){
+                for(y=rect.dy; y<rect.dy+8;y++){
+                    display[320*y + x] = 0xffff;
                 }
             }
-            ioctl(fdfb, 0x4680, &rect);
-
-            //Update snake
-            for (x=snakeLength;x>0;x--){
-                snake[x] = snake[x-1];
+            ioctl(fd_fb, 0x4680, &rect);
+            /*
+            printf("snake: snakeLength: %d snakeTail: %d\n",snakeLength,snakeTail );
+            for(x=snakeTail;x<snakeTail + snakeLength;x++){
+                if ((x) < 400){
+                    printf("[%d , %d]\n",snake[x].x,snake[x].y );
+                }else{
+                   printf("[%d , %d]\n",snake[x-400].x,snake[x-400].y );
+                }
             }
-            snake[0] = next;
+            */
+            usleep(1000*100 - speed*10*1000);
+            
+            while(pauseGame == 1){
+                usleep(1000*100);
+
+            }
+            
 
 
         }
-        //Generate head
-        rect.dx = next.x;
-        rect.dy = next.y;
-        for(x=rect.dx; x<rect.dx+9; x++){
-            for(y=rect.dy; y<rect.dy+8;y++){
-                display[320*y + x] = 0xffff;
-            }
-        }
-        ioctl(fdfb, 0x4680, &rect);
+        // because we did not take the time to make text for the display.
+        printf("Your score: %d\n",snakeLength);
+        
 
-
-        usleep(1000*100 - speed*200);
     }
-    printf("Your score: %d\n",snakeLength);
-    free(snake);
     munmap(display,320*240*2);
-    close(fdfb);
-    close(fdrandom);
-    close(fddriver);
+    close(fd_fb);
+    close(fd_random);
+    close(fd_gamepad);
 	exit(EXIT_SUCCESS);
 }
 
-void readGamepad(int fddriver,pos *dir){
- 
-    uint8_t buffer;
-    read(fddriver,&buffer,1);
-  
-    if (dir->x == 0){
-        if(buffer == 1){
-            dir->y = 0;
-            dir->x = -1;
-        }
-        else if(buffer == 3){
-            dir->y = 0;
-            dir->x = 1;
-        }
-    }else{
-        if(buffer == 2){
-            dir->y = 1;
-            dir->x = 0;
-        }
-        else if(buffer == 4){
-            dir->y = -1;
-            dir->x = 0;
-        }
-    }
 
-    return;
+void sig_handler(int sig_num, siginfo_t *info, void *unused){
+
+    uint8_t buffer = info->si_int;
+
+    switch(buffer | 0xff00){
+        case (0xfffe) : // 1
+            if (dir.x == 0){
+                dir.y = 0;
+                dir.x = -1; 
+            }
+            return;
+        case (0xfffd) : // 2
+            if(dir.y == 0){
+                dir.y = -1;
+                dir.x = 0; 
+            }
+            return;
+        case (0xfffb) : // 3
+            if (dir.x == 0){
+                dir.y = 0;
+                dir.x = 1;
+            }
+            return;
+        case (0xfff7) : // 4
+            if(dir.y == 0){
+                dir.y = 1;
+                dir.x = 0;
+            }
+            return;
+        case (0xffef) : // 5
+            pauseGame = abs(pauseGame-1);
+            return;
+            
+        case (0xffdf) : // 6
+            if(speed < 9){
+                speed++;
+            }    
+            return;
+            
+        case (0xffbf) : // 7
+            reset = 1;
+            return;
+        case (0xff7f) : // 8
+            if(speed > 1){
+                speed--;    
+            }
+            return;
+        default:
+            return;    
+        }
+
+
 }
 
 
-void newApple(uint16_t* display,int fdrandom, int fdfb,pos* apple,pos* next, struct fb_copyarea *rect){
+
+
+void newApple(uint16_t* display,int fd_random, int fd_fb,pos* apple,pos* next, struct fb_copyarea *rect){
     unsigned int random;
     unsigned int appleFound = 1;
     int x,y;    
     while(appleFound != 0){
 
         appleFound++;
-        read(fdrandom,&random,5);
+        read(fd_random,&random,5);
         apple->x = 10*(random % 32);
-        read(fdrandom,&random,5);
+        read(fd_random,&random,5);
         apple->y = 10*(random % 24);
 
 
@@ -206,7 +294,7 @@ void newApple(uint16_t* display,int fdrandom, int fdfb,pos* apple,pos* next, str
                     display[320*y + x] = 0xf000;
                 }
             }
-            ioctl(fdfb, 0x4680, rect);
+            ioctl(fd_fb, 0x4680, rect);
             break;
         }else if(appleFound>1000){
             printf("Can't find location for a new apple, exiting\n");
